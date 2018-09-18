a0bfb63de1a03d6a4d950c2133ead69add222c43
Lee Richmond <lrichmond1@bloomberg.net>
2018-09-18 13:26:48 -0400

    Add data grid

---
+++ src/components/Query.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card query">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.firstName }} {{ employee.lastName }}</td>
+          <td>{{ employee.age }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Employee } from "@/models"
+
+export default Vue.extend({
+  name: 'query',
+  data() {
+    return {
+      employees: [] as Employee[]
+    }
+  },
+  created() {
+    this.search()
+  },
+  methods: {
+    async search() {
+      this.employees = (await Employee.all()).data
+    }
+  }
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
+    <h1>Employee Directory</h1>
+    <query />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Query from "@/components/Query.vue"
 
 export default Vue.extend({
   name: 'home',
+  components: {
+    Query
+  }
 });
 </script>
