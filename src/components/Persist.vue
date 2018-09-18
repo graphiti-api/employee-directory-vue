b2762a56deb757cde433665d8e2247311d687726
Lee Richmond <lrichmond1@bloomberg.net>
2018-09-18 14:31:06 -0400

    Add form

---
+++ src/components/Persist.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="persist card form clearfix">
+    <h3 class="text-white">Create New Employee</h3>
+      <form @submit.prevent="submit()">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="First Name" />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Age" />
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Last Name" />
+          </div>
+        </div>
+      </div>
+        <div class="positions">
+        <div v-for="(position, index) in employee.positions" :key="position.id" class="row position">
+          <div class="col-md-4">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Title" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <select class="form-control">
+                <option disabled :value="undefined">Select a Department</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+        <button type="submit" class="float-right btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Employee, Position } from "@/models"
+
+export default Vue.extend({
+  name: 'persist',
+  data () {
+    let employee = new Employee({
+      positions: [new Position()]
+    })
+
+    return { employee }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
     <h1>Employee Directory</h1>
-    <query />
+
+    <div class="row">
+      <div class="col-md-6">
+        <query />
+      </div>
+      <div class="col-md-6">
+        <persist />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Query from "@/components/Query.vue"
+import Persist from "@/components/Persist.vue"
 
 export default Vue.extend({
   name: 'home',
   components: {
-    Query
+    Query,
+    Persist
   }
 });
 </script>
