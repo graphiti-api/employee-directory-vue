7dfafdc910de536a647783449df7843d9b4ef069
Lee Richmond <lrichmond1@bloomberg.net>
2018-09-18 13:15:51 -0400

    Define models

---
+++ src/models.ts
@@ -0,0 +1,45 @@
+import {
+  SpraypaintBase,
+  Model,
+  Attr,
+  HasMany,
+  HasOne,
+  BelongsTo
+} from "spraypaint"
+
+@Model()
+export class ApplicationRecord extends SpraypaintBase {
+  static baseUrl = "/"
+  static apiNamespace = "api/v1"
+}
+
+@Model()
+export class Employee extends ApplicationRecord {
+  static jsonapiType = "employees"
+
+  foos: string[]
+
+  @Attr() firstName: string
+  @Attr() lastName: string
+  @Attr() age: number
+
+  @HasMany() positions: Position[]
+  @HasOne() currentPosition: Position
+}
+
+@Model()
+export class Position extends ApplicationRecord {
+  static jsonapiType = "positions"
+
+  @Attr() title: string
+  @Attr() historicalIndex: number
+
+  @BelongsTo() department: Department
+}
+
+@Model()
+export class Department extends ApplicationRecord {
+  static jsonapiType = "departments"
+
+  @Attr() name: string
+}
